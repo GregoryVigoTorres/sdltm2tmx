e5d25c4b86cc5e1c838068ec98a265a463f080d8
Gregory Vigo Torres <gregory.vigo@gmail.com>
2017-02-21 03:09:21 +0100

    packaged

--- sdltm2tmx/__init__.py
+++ sdltm2tmx/__init__.py
@@ -1,9 +1,7 @@
-#! /usr/bin/env python3
-
-from sdltm2tmx import run
+from .sdltm2tmx import run
 
 
-if __name__ == '__main__':
+def main():
     src = input('Path to sdltm > ')
     tmx_save_root = input('Directory to save tmx in > ')
     run(src, tmx_save_root)
