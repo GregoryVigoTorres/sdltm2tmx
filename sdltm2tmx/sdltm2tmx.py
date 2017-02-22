02ce0101e2f126d5ea32134095f3115371d9c25b
Gregory Vigo Torres <gregory.vigo@gmail.com>
2017-02-22 18:27:27 +0100

    Log tm source path to stdout

--- sdltm2tmx/sdltm2tmx.py
+++ sdltm2tmx/sdltm2tmx.py
@@ -139,6 +139,7 @@ def get_translation_memory_props(c):
 def run(src, tmx_save_root):
     src = src
     tmx_save_root = tmx_save_root
+    print('opening tm: {}'.format(src))
 
     conn = sqlite3.connect(src)
     conn.row_factory = sqlite3.Row
