664411f5a1788bc9f34f4d93c77d783afd57ac76
Gregory Vigo Torres <>
2018-08-14 19:46:08 +0200

    got setup to work with gitlab repo

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name='sdltm2tmx',
-    version='0.1',
+    version='1.0',
     description='convert sdltm translation memories to tmx1.4',
     author='Gregory Vigo Torres',
     license='gplv3',
@@ -12,9 +13,16 @@ setup(
     ],
     keywords='translation localization',
     packages=find_packages(),
+    install_requires=[
+        'tmx-writer==1.1',
+        'click'
+    ],
+    dependency_links=[
+        'git+https://gitlab.com/grgvt/tmx_writer.git#egg=tmx-writer-1.1',
+    ],
     entry_points={
         'console_scripts': [
-            'sdltm2tmx-cli=sdltm2tmx:main'
+            'sdltm2tmx=sdltm2tmx.sdltm2tmx_cli:main'
         ],
     },
 )
