5fc946548f097552bddbf766f98bbc7bfda89b67
Gregory Vigo Torres <>
2022-05-31 11:56:11 +0200

    build and installation improvements

--- setup.py
+++ setup.py
@@ -1,27 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
-VERSION = '2.0'
-
-setup(
-    name='sdltm2tmx',
-    version=VERSION,
-    description='convert sdltm translation memories to tmx1.4',
-    author='Gregory Vigo Torres',
-    license='gplv3',
-    classifiers=[
-        'Development Status :: 4 Beta',
-        'Programming Language :: Python :: 3'
-    ],
-    keywords='translation localization',
-    packages=find_packages(),
-    install_requires=[
-        'click',
-        'lxml'
-    ],
-    entry_points={
-        'console_scripts': [
-            'sdltm2tmx=sdltm2tmx.sdltm2tmx_cli:main'
-        ],
-    },
-)
+setup()
